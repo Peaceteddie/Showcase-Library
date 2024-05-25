12ddffae61f065985a42177d4228beebfa69bcd0
Peaceteddie <Peaceteddie@gmail.com>
2024-05-25 20:12:45 +0200

    Added game status and fixed restart

--- Extensions.cs
+++ Extensions.cs
@@ -4,8 +4,15 @@ public static class Extensions
 {
     public static T Inspect<T>(this T item)
     {
-        var json = JsonSerializer.Serialize(item);
-        Console.WriteLine(json);
+        try
+        {
+            var json = JsonSerializer.Serialize(item);
+            Console.WriteLine(json);
+        }
+        catch
+        {
+            Console.WriteLine($"Not serializable: {item}");
+        }
         return item;
     }
 }
